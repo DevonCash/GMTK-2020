e0948f4512f73dc0b15c179752b339c77efb0c15
moehnked <40707466+moehnked@users.noreply.github.com>
2020-07-11 12:53:00 -0700

    finished terminal prototype

--- Obstacles/Terminal/Terminal.gd
+++ Obstacles/Terminal/Terminal.gd
@@ -4,11 +4,23 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var health = 4
+enum State {ready, hacking, destroyed}
+var state = State.ready
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite.set_animation("default")
+	state = State.ready
 
 func onHacked(who, damage, knockback):
 	#queue up hacking prompt
+	print("hacked")
+	if damage:
+		health -= damage;
+		if state == State.ready:
+			state = State.hacking
+			$AnimatedSprite.set_animation("hacking")
+		if health <= 0: self.queue_free()
+		
+		
