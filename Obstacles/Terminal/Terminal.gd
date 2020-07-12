2c9d60b48a45554cd45f923cadd087328b89830d
moehnked <40707466+moehnked@users.noreply.github.com>
2020-07-11 19:22:45 -0700

    tested the terminal

--- Obstacles/Terminal/Terminal.gd
+++ Obstacles/Terminal/Terminal.gd
@@ -5,22 +5,33 @@ extends StaticBody2D
 # var a = 2
 # var b = "text"
 var health = 4
-enum State {ready, hacking, destroyed}
+enum State {ready, hacking, destroyed, hit}
 var state = State.ready
+var origin = position
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.set_animation("default")
 	state = State.ready
+	origin = position
+
+func _process(delta):
+	match state:
+		State.hit:
+			$AnimatedSprite.set_offset(Vector2(rng.randi_range(-3,3),rng.randi_range(-3,3)))
+			state = State.hacking
+		State.hacking:
+			position = origin
 
 func onHacked(who, damage, knockback):
 	#queue up hacking prompt
 	print("hacked")
 	if damage:
 		health -= damage;
+		state = State.hit
 		if state == State.ready:
-			state = State.hacking
-			$AnimatedSprite.set_animation("hacking")
+			$AnimatedSprite.set_animation("hacked")
 		if health <= 0: self.queue_free()
 		
 		
