6ceee4f0ecc414b1d28ff37733fc960c35165396
moehnked <40707466+moehnked@users.noreply.github.com>
2020-07-11 21:58:29 -0700

    fixed bugs

--- Player/DashEffect.gd
+++ Player/DashEffect.gd
@@ -7,14 +7,11 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _process(delta):
+	if($AnimatedSprite.frame == 4):
+		queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
-
-
-func _on_AnimatedSprite_animation_finished():
-	queue_free()
+#	pas
